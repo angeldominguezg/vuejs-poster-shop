f9cfbf46c4c3f3eff5dd18e2ce221d3519c32722
Angelddg <angeldominguezg@gmail.com>
2018-04-15 14:31:26 -0400

    Add to cart functionality

--- public/script.js
+++ public/script.js
@@ -1,1 +1,19 @@
-console.log('It works.');
+new Vue ({
+  el: '#app',
+  data() {
+    return {
+      total: 0,
+      items: [
+        {title: 'Item 1'},
+        {title: 'Item 2'},
+        {title: 'Item 3'}
+      ]
+    }
+  },
+  methods: {
+    addItem () {
+      console.log('[addItem]', this.articleName)
+    }
+  }
+
+})
